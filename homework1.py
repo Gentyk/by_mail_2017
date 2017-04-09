e717326fc43f4f27e1a42d68f62b655d3df6995d
Gentyk <33gva@mail.ru>
2017-04-09 13:42:13 +0300

    version good

--- homework1.py
+++ homework1.py
@@ -4,35 +4,56 @@ import datetime
 
 class Task:
     def __init__(self,title:str,estimate:datetime.date,state="in_progress"):
+        if type(title)!=str:
+            raise TypeError("Invalid data for the attribute TITLE: must be str.")
         self.title=title
+        if type(estimate)!=datetime.date:
+            raise TypeError("Invalid data for the attribute ESTIME: must be datetime.date.")
         self.estimate=estimate
+        if type(state)!=str:
+            raise TypeError("Invalid data for the attribute STATE: must be str.")
+        if state!="in_progress" and state!="ready":
+            raise AttributeError
         self.state=state
-    def remaining(self):
+
+    @property
+    def remaining(self)->datetime.timedelta:
         if self.state=="in_progress":
             return self.estimate-datetime.today()
         else:
             return 0
+
+    @property
     def is_failed(self):
         if self.state=="in_progress" and self.estimate<datetime.today():
             return True
         else:
             return False
+
     def ready(self):
         self.state="ready"
 
-from typing import List
-class Roadmap:
-    t_tasks=List[Task]
 
+class Roadmap:
     def __init__(self,tasks=[]):
-        self.tasks=tasks
+        if all(isinstance(x, Task) for x in tasks)==False:
+            raise TypeError("tasks must have type=List[Task]")
+        else:
+            self.tasks = tasks
+
+
+
+    @property
     def today(self):
         tasks_new=List[Task]
         for t in self.tasks:
             if t.estimate==datetime.today():
                 tasks_new.append(t)
         return tasks_new
+
     def filter(self,state:str):
+        if type(state)!=str:
+            raise TypeError("")
         tasks_new = List[Task]
         for t in self.tasks:
             if t.state==state:
@@ -40,13 +61,14 @@ class Roadmap:
         return tasks_new
 
 
-"""a=Task("1",datetime.datetime(2050,11,10))
-b=Task("2",datetime.datetime(1996,11,10))
+a=Task("1",datetime.date(2050,11,10))
+b=Task("2",datetime.date(1996,11,10))
 b.ready()
 c=[a,b]
-print(str(c[0].estimate))
+#c=[a,b,"dlllssdsd"]
+#print(str(c[0].estimate))
 d=Roadmap(c)
-print(d.tasks[0].state)#"""
+print(d.tasks[1].state)
 
 
 
